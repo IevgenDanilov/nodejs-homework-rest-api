8a2c5eef5cd952543ecb449b0de4746873ee08df
Yevhen Danilov <danilov.evgen@gmail.com>
2021-11-17 23:28:38 +0200

    added some functions

--- model/index.js
+++ model/index.js
@@ -1,15 +1,28 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+// const contacts = require("./contacts.json");
+const filePath = require("./contactsPath");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = await fs.readFile(filePath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => { };
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === Number(contactId));
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -17,4 +30,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const contactsOperations = require("../../model");
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template1 message" });
+  const list = await contactsOperations.listContacts();
+  res.json(list);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contact = await contactsOperations.getContactById(contactId);
+  if (!contact) {
+    throw new Error(`Not found contact with id=${contactId}`);
+  }
+  // console.log("Contact was found:\n", contact);
+  // return contact;
+  res.json({ status: "success", message: "Contact was found:\n", contact });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  res.json({
+    status: "success",
+    message: "Contact add success",
+  });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
